b10f5d83c94fb9cb39e777661ce5c6f5bb1af00a
k.dubov <example@example.com>
2022-09-13 23:29:42 +0300

    Update privacy page

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -1,21 +1,21 @@
 import * as React from "react"
 
-import Layout, {Section} from "../components/layout"
+import Layout, { Section } from "../components/layout"
 import Seo from "../components/seo"
 
-const SinglePost = ({pageContext}) => (
+const SinglePost = ({ pageContext }) => (
     <Layout>
-        {console.log(pageContext.html)}
         <Section content={`content`}>
-            <div  dangerouslySetInnerHTML={{__html: pageContext.html}}
-                  style={{
-                      fontFamily: 'Noto Sans',
-                      fontStyle: "normal",
-                      fontWeight: "400",
-                      fontSize: '16px',
-                      lineHeight: "24px",
-                      color: "#FFFFFF"
-                  }}></div>
+            <div dangerouslySetInnerHTML={{ __html: pageContext.html }}
+                style={{
+                    fontFamily: 'Open Sans',
+                    fontStyle: "normal",
+                    fontWeight: "400",
+                    fontSize: '16px',
+                    lineHeight: "24px",
+                    color: "rgba(0, 0, 0, 0.64)"
+                }}>
+            </div>
         </Section>
     </Layout>
 );
