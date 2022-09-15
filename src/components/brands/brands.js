f44abdc6148e53fedcec9965fc3d17ed14cf48c9
k.dubov <example@example.com>
2022-09-15 18:35:29 +0300

    Update brands

--- src/components/brands/brands.js
+++ src/components/brands/brands.js
@@ -4,16 +4,18 @@ import brand2 from "../../images/Swintt.svg";
 import brand3 from "../../images/KAGaming.svg";
 import brand4 from "../../images/GO.svg";
 
-const brands = [brand1, brand2, brand3, brand4]
+const brands = [{brand: brand1, url: "https://mascot.games/"}, {brand: brand2, url: "https://www.swintt.com/games/"}, {brand: brand3, url: "https://www.spadegaming.com/games"}, {brand: brand4, url: "https://www.redtiger.com/"}]
 
 const Brands = () => {
     return (
         <div className={`brands vw-grid`}>
             <h2 className={`brands-title`}>Trendy brands:</h2>
             <ul className={`brands-list list-style-reset`}>
-                {brands.map((brand, index) => (
+                {brands.map(({ brand, url }, index) => (
                     <li className={`brands-item`} key={index}>
-                        <img className={`brands-item-logo`} src={brand} alt="Brand"/>
+                        <a href={url} target="_blank" rel="noreferrer" className='brands-item-link'>
+                            <img className={`brands-item-logo`} src={brand} alt="Brand" />
+                        </a>
                     </li>
                 ))}
             </ul>
@@ -21,4 +23,4 @@ const Brands = () => {
     )
 };
 
-export {Brands as default};
+export { Brands as default };
